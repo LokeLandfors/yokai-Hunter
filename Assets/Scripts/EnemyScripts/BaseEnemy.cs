6bce32ee5543f818d706950ef16fc2e9d6dfbb82
Freakbert <edwin.skoglund@elev.ga.lbs.se>
2025-11-07 10:19:31 +0100

    full enemy dmg fix

--- Assets/Scripts/EnemyScripts/BaseEnemy.cs
+++ Assets/Scripts/EnemyScripts/BaseEnemy.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
+using UnityEditor;
+using Unity.VisualScripting;
 
 public class BaseEnemy : EnemyCoreLogic
 {
@@ -22,26 +24,47 @@ public class BaseEnemy : EnemyCoreLogic
 
     public override void MeleeAttack()
     {
-        meleeDetect();
+        print("POW");
+        if (activeMeleeCool > 0 || activeMelee) return;
+
+        //activeMelee = true;
+        StartCoroutine(meleeDetect());
     }
 
     IEnumerator meleeDetect()
     {
-        print("POW");
-        if (activeMeleeCool > 0) yield break;
-        activeMelee = true;
-        yield return new WaitForSecondsRealtime(meleeDelay);
-        Collider2D hitObj = Physics2D.OverlapBox(new Vector2(hitboxoffset * walkDir, 0), hitboxsize, 0, 8);
-        print(hitObj);
-        activeMeleeCool = meleeCool;
-        if (hitObj == null)
         {
-            yield break;
+            activeMelee = true; // Set flag to indicate the attack is in progress
+            yield return new WaitForSecondsRealtime(meleeDelay); // Wait for the delay (animation or cooldown)
+
+            // Only perform the attack once (this is to prevent continuous damage)
+            Collider2D hitObj = Physics2D.OverlapBox(
+                (Vector2)transform.position + new Vector2(hitboxoffset * walkDir, 0),
+                hitboxsize,
+                0,
+                playerLayer
+            );
+
+            print(hitObj);
+
+            if (hitObj != null && hitObj.CompareTag("Player"))
+            {
+                hitObj.GetComponent<PlayerRespawn>().playerHealth -= meleeDmg; // Deal damage to player
+            }
+
+            // After the attack, reset the cooldown and the attacking flag
+            activeMeleeCool = meleeCool;
+            activeMelee = false; // Attack is done
         }
-        else
+
+        void OnDrawGizmos()
         {
-            hitObj.GetComponent<PlayerRespawn>().playerHealth -= meleeDmg;
+            if (activeMelee)
+            {
+                Gizmos.color = Color.red;
+                Vector2 colliderCenter = (Vector2)transform.position + new Vector2(hitboxoffset * walkDir, 0); // Position of the collider
+                Gizmos.DrawWireCube(colliderCenter, hitboxsize); // Size and position of the box
+            }
         }
     }
-
 }
