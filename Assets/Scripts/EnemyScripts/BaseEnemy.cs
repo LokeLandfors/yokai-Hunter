670c493c62ef243076c7344a47ef1e9945db4553
Freakbert <edwin.skoglund@elev.ga.lbs.se>
2025-11-07 12:56:11 +0100

    new enemies

--- Assets/Scripts/EnemyScripts/BaseEnemy.cs
+++ Assets/Scripts/EnemyScripts/BaseEnemy.cs
@@ -5,26 +5,32 @@ using UnityEditor;
 using Unity.VisualScripting;
 
 public class BaseEnemy : EnemyCoreLogic
+    //så jag typ återanvänder den här koden till alla fiender istället för att uppdatera enemy logic för orka asså holy shit
 {
     [SerializeField] Vector2 hitboxsize;
     [SerializeField] float hitboxoffset;
     [SerializeField] float meleeDmg;
     [SerializeField] float meleeDelay; //hur länge tills försöka skada
 
+    SpriteRenderer sprite;
+    Animator animator;
+
     public override void Start()
     {
         base.Start();
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+        animator = gameObject.GetComponent<Animator>();
     }
 
     public override void Update()
     {
+        sprite.flipX = !faceright;
         base.Update();
         activeMeleeCool -= Time.deltaTime;
     }
 
     public override void MeleeAttack()
     {
-        print("POW");
         if (activeMeleeCool > 0 || activeMelee) return;
 
         //activeMelee = true;
@@ -34,20 +40,18 @@ public class BaseEnemy : EnemyCoreLogic
     IEnumerator meleeDetect()
     {
         {
-            activeMelee = true; // Set flag to indicate the attack is in progress
-            yield return new WaitForSecondsRealtime(meleeDelay); // Wait for the delay (animation or cooldown)
+            animator.SetBool("Attacking", true); //spela attack animation
 
-            // Only perform the attack once (this is to prevent continuous damage)
+            activeMelee = true;
+            yield return new WaitForSecondsRealtime(meleeDelay); //vänta attackdelay tiden innan hitbox sätts på
+
             Collider2D hitObj = Physics2D.OverlapBox(
-                (Vector2)transform.position + new Vector2(hitboxoffset * walkDir, 0),
-                hitboxsize,
-                0,
-                playerLayer
-            );
+                (Vector2)transform.position + new Vector2(hitboxoffset * walkDir, 0) , hitboxsize, 0, playerLayer);
+            
 
             print(hitObj);
 
-            if (hitObj != null && hitObj.CompareTag("Player"))
+            if (hitObj != null && hitObj.CompareTag("Player") && !hitObj.GetComponent<PlayerMovement>().isDashing)
             {
                 hitObj.GetComponent<PlayerRespawn>().playerHealth -= meleeDmg; // Deal damage to player
             }
@@ -55,6 +59,7 @@ public class BaseEnemy : EnemyCoreLogic
             // After the attack, reset the cooldown and the attacking flag
             activeMeleeCool = meleeCool;
             activeMelee = false; // Attack is done
+            animator.SetBool("Attacking", false);
         }
 
         void OnDrawGizmos()
--- Assets/Scripts/EnemyScripts/ShadowWalkerLogic.cs
+++ Assets/Scripts/EnemyScripts/ShadowWalkerLogic.cs
@@ -4,34 +4,6 @@ using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ShadowWalkerLogic : EnemyCoreLogic
+public class ShadowWalkerLogic : BaseEnemy
 {
-    /*public int damage = 2; //Skada som spelaren ska ta när Enemy rör honom
-    public float attackTime = 2f;
-    protected virtual bool MeleeAttack(bool returnFoundHit)
-    {
-        return false;
-    }
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            PlayerRespawn player = collision.gameObject.GetComponent<PlayerRespawn>();
-
-            if (player != null)
-            {
-                player.TakeDamage(damage);
-            }
-        }
-    }
-    private IEnumerator EnemyMeleeAttack()
-    {
-        bool hit = false;
-        while (activeMeleeCool > 0)
-        {
-            MeleeAttack(hit);
-        activeMeleeCool -= Time.deltaTime;
-        yield return new WaitForSeconds(Time.deltaTime);
-        }
-    }*/
 }
